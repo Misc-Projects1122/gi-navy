0f078de366d4aaa35fe79628e91780eecbb28171
Vedang1358 <2021.vedang.rathi@ves.ac.in>
2024-07-09 19:37:44 +0530

    what we do updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,17 @@ module.exports = {
         sans: ["Poppins", "sans-serif"],
         barlow: ["'Barlow Condensed'", "sans-serif"],
       },
+      colors: {
+        dark: "#03111E",
+        backgroundSec: "#EEFBFB",
+        background: "#0E1D2B",
+        primary: "#01BDB7",
+        secondary: "#007BC7",
+      },
+      dropShadow: {
+        'white': '5px 0 0 #ffffff',
+        'primary':'0 0 5px #01BDB7',
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
