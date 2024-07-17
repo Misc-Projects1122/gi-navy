8629805ff7808ec1c0323718e5d0207bce216605
Vedang1358 <2021.vedang.rathi@ves.ac.in>
2024-07-17 13:40:06 +0530

    what we do updates

---
+++ src/screens/what-we-do/cards.ts
@@ -0,0 +1,109 @@
+export const enginnerItemCards = [
+  {
+    imgSrc: "/src/assets/solutions1.png",
+    altText: "INS Jyoti",
+    title: "INS Jyoti",
+    desc: "Komandarm Fedko class fleet replenishment oiler",
+    bgColor: "bg-background",
+    textColor: "text-primary",
+    descColor: "text-white",
+  },
+  {
+    imgSrc: "/src/assets/solutions2.png",
+    altText: "INS Beas",
+    title: "INS Beas",
+    desc: "Bhahmaputra class frigate",
+    bgColor: "bg-white",
+    textColor: "text-secondary",
+    descColor: "text-background",
+  },
+  {
+    imgSrc: "/src/assets/solutions3.png",
+    altText: "INS Nireekshak",
+    title: "INS Nireekshak",
+    desc: "Diving support vessel",
+    bgColor: "bg-background",
+    textColor: "text-primary",
+    descColor: "text-white",
+  },
+  {
+    imgSrc: "/src/assets/solutions4.png",
+    altText: "MT Swarna Kalash",
+    title: "MT Swarna Kalash",
+    desc: "Oil products tanker",
+    bgColor: "bg-white",
+    textColor: "text-secondary",
+    descColor: "text-background",
+  },
+  {
+    imgSrc: "/src/assets/solutions5.png",
+    altText: "INS Tarangini and INS Sudarshini",
+    title: "INS Tarangini and INS Sudarshini",
+    desc: "Sail training ship",
+    bgColor: "bg-background",
+    textColor: "text-primary",
+    descColor: "text-white",
+  },
+  {
+    imgSrc: "/src/assets/solutions6.png",
+    altText: "MV Arabian Sea",
+    title: "MV Arabian Sea",
+    desc: "General passenger/cargo ship",
+    bgColor: "bg-white",
+    textColor: "text-secondary",
+    descColor: "text-background",
+  },
+];
+
+export const criticalItemCards = [
+  {
+    imgSrc: "/src/assets/structure1.png",
+    altText: "Expert Design Capabilities",
+    title: "Expert Design Capabilities",
+    desc: `Leveraging extensive experience, we design helidecks tailored to
+            the unique specifications, spatial constraints, and operational
+            requirements of various naval vessels. Our design team employs
+            state-of-the-art software and simulation tools to ensure optimal
+            functionality, safety, and integration with onboard systems.`,
+  },
+  {
+    imgSrc: "/src/assets/structure2.png",
+    altText: "Precision Fabrication Techniques",
+    title: "Precision Fabrication Techniques",
+    desc: `At G1, we use advanced techniques, cutting-edge materials, and
+            strict quality control to fabricate durable, reliable, and
+            high-performance helidecks. Our structures undergo rigorous
+            testing for structural integrity and corrosion resistance,
+            exceeding industry and regulatory standards.`,
+  },
+  {
+    imgSrc: "/src/assets/structure3.png",
+    altText: "Seamless Installation Processes",
+    title: "Seamless Installation Processes",
+    desc: `Our skilled installation teams expertly execute helideck
+            installations with meticulous attention to detail, efficiency, and
+            safety. Whether retrofitting existing vessels or integrating into
+            new platforms, we ensure seamless coordination, adherence to
+            project timelines, and minimal disruption to ongoing operations.`,
+  },
+  {
+    imgSrc: "/src/assets/structure4.png",
+    altText: "Compliance and Certification",
+    title: "Compliance and Certification",
+    desc: `We ensure all helidecks designed, fabricated, and installed by G1
+            comply with the strictest standards set by relevant authorities,
+            including international maritime organizations and naval oversight
+            bodies. This commitment underscores our dedication to safety,
+            reliability, and operational excellence.`,
+  },
+  {
+    imgSrc: "/src/assets/structure5.png",
+    altText: "Continuous Support and Maintenance",
+    title: "Continuous Support and Maintenance",
+    desc: `Beyond installation, G1 is committed to ongoing support,
+            maintenance, and lifecycle management to ensure helideck
+            performance and longevity. Our responsive support teams are
+            available 24/7 to address emergent needs, conduct inspections, and
+            implement proactive maintenance measures.`,
+  },
+];
