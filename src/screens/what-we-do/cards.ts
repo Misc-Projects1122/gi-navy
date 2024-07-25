9895f52c87ce9aa480db56bd167fc576f60fb3ea
micksharma <134577866+ansul-rathi@users.noreply.github.com>
2024-07-25 10:24:22 +0530

    images update

--- src/screens/what-we-do/cards.ts
+++ src/screens/what-we-do/cards.ts
@@ -1,6 +1,6 @@
 export const enginnerItemCards = [
   {
-    imgSrc: "/assets/solutions1.png",
+    imgSrc: "/assets/solutions1.webp",
     altText: "INS Jyoti",
     title: "INS Jyoti",
     desc: "Komandarm Fedko class fleet replenishment oiler",
@@ -9,7 +9,7 @@ export const enginnerItemCards = [
     descColor: "text-white",
   },
   {
-    imgSrc: "/assets/solutions2.png",
+    imgSrc: "/assets/solutions2.webp",
     altText: "INS Beas",
     title: "INS Beas",
     desc: "Bhahmaputra class frigate",
@@ -18,7 +18,7 @@ export const enginnerItemCards = [
     descColor: "text-background",
   },
   {
-    imgSrc: "/assets/solutions3.png",
+    imgSrc: "/assets/solutions3.webp",
     altText: "INS Nireekshak",
     title: "INS Nireekshak",
     desc: "Diving support vessel",
@@ -27,7 +27,7 @@ export const enginnerItemCards = [
     descColor: "text-white",
   },
   {
-    imgSrc: "/assets/solutions4.png",
+    imgSrc: "/assets/solutions4.webp",
     altText: "MT Swarna Kalash",
     title: "MT Swarna Kalash",
     desc: "Oil products tanker",
@@ -36,7 +36,7 @@ export const enginnerItemCards = [
     descColor: "text-background",
   },
   {
-    imgSrc: "/assets/solutions5.png",
+    imgSrc: "/assets/solutions5.webp",
     altText: "INS Tarangini and INS Sudarshini",
     title: "INS Tarangini and INS Sudarshini",
     desc: "Sail training ship",
@@ -45,7 +45,7 @@ export const enginnerItemCards = [
     descColor: "text-white",
   },
   {
-    imgSrc: "/assets/solutions6.png",
+    imgSrc: "/assets/solutions6.webp",
     altText: "MV Arabian Sea",
     title: "MV Arabian Sea",
     desc: "General passenger/cargo ship",
@@ -58,7 +58,7 @@ export const enginnerItemCards = [
 export const criticalItemCards = [
   {
     index: 1,
-    imgSrc: "/assets/structure1.png",
+    imgSrc: "/assets/structure1.webp",
     altText: "Expert Design Capabilities",
     title: "Expert Design Capabilities",
     desc: `Leveraging extensive experience, we design helidecks tailored to
@@ -69,7 +69,7 @@ export const criticalItemCards = [
   },
   {
     index: 2,
-    imgSrc: "/assets/structure2.png",
+    imgSrc: "/assets/structure2.webp",
     altText: "Precision Fabrication Techniques",
     title: "Precision Fabrication Techniques",
     desc: `At G1, we use advanced techniques, cutting-edge materials, and
@@ -80,7 +80,7 @@ export const criticalItemCards = [
   },
   {
     index: 3,
-    imgSrc: "/assets/structure3.png",
+    imgSrc: "/assets/structure3.webp",
     altText: "Seamless Installation Processes",
     title: "Seamless Installation Processes",
     desc: `Our skilled installation teams expertly execute helideck
@@ -91,7 +91,7 @@ export const criticalItemCards = [
   },
   {
     index: 4,
-    imgSrc: "/assets/structure4.png",
+    imgSrc: "/assets/structure4.webp",
     altText: "Compliance and Certification",
     title: "Compliance and Certification",
     desc: `We ensure all helidecks designed, fabricated, and installed by G1
@@ -102,7 +102,7 @@ export const criticalItemCards = [
   },
   {
     index: 5,
-    imgSrc: "/assets/structure5.png",
+    imgSrc: "/assets/structure5.webp",
     altText: "Continuous Support and Maintenance",
     title: "Continuous Support and Maintenance",
     desc: `Beyond installation, G1 is committed to ongoing support,
