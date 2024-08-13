831abce0596845154abb8d7bc0a519d2e105fd95
micksharma <134577866+ansul-rathi@users.noreply.github.com>
2024-08-13 18:47:07 +0530

    mail chimp fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+// https://astro.build/config/
 export default defineConfig({
   output: "server",
   adapter: vercel(),
